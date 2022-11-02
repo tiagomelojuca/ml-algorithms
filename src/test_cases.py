80b499da97a40523f50fd50c7c89f0d729d0c881
Tiago Melo Juca <contact@tiagomelojuca.com>
2022-11-02 04:33:28 -0300

    add CMU Face Images dataset

--- src/test_cases.py
+++ src/test_cases.py
@@ -6,6 +6,7 @@ import os
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 ###############################################################################
 
 def DUMMYREG_getData():
@@ -220,5 +221,92 @@ def DUMMYMULTICLS_showData():
     plt.xlim((-1, 7))
     plt.ylim((-1, 7))
     plt.show()
+
+###############################################################################
+
+def CMUFACEIMAGES_getData(Red):
+    folderRoot = "datasets\\faces\\faces\\"
+
+    individual = [
+        "an2i",
+        "at33",
+        "boland",
+        "bpm",
+        "ch4f",
+        "cheyer",
+        "choon",
+        "danieln",
+        "glickman",
+        "karyadi",
+        "kawamura",
+        "kk49",
+        "megak",
+        "mitchell",
+        "night",
+        "phoebe",
+        "saavik",
+        "steffi",
+        "sz24",
+        "tammo"
+    ]
+
+    expressoes = [
+        "_left_angry_open",
+        "_left_angry_sunglasses",
+        "_left_happy_open",
+        "_left_happy_sunglasses",
+        "_left_neutral_open",
+        "_left_neutral_sunglasses",
+        "_left_sad_open",
+        "_left_sad_sunglasses",
+        "_right_angry_open",
+        "_right_angry_sunglasses",
+        "_right_happy_open",
+        "_right_happy_sunglasses",
+        "_right_neutral_open",
+        "_right_neutral_sunglasses",
+        "_right_sad_open",
+        "_right_sad_sunglasses",
+        "_straight_angry_open",
+        "_straight_angry_sunglasses",
+        "_straight_happy_open",
+        "_straight_happy_sunglasses",
+        "_straight_neutral_open",
+        "_straight_neutral_sunglasses",
+        "_straight_sad_open",
+        "_straight_sad_sunglasses",
+        "_up_angry_open",
+        "_up_angry_sunglasses",
+        "_up_happy_open",
+        "_up_happy_sunglasses",
+        "_up_neutral_open",
+        "_up_neutral_sunglasses",
+        "_up_sad_open",
+        "_up_sad_sunglasses"
+    ]
+
+    QtdIndividuos = len(individual)
+    QtdExpressoes = len(expressoes)
+    X = np.empty((Red * Red, 0))
+    Y = np.empty((QtdIndividuos, 0))
+
+    for i in range (QtdIndividuos):
+        for j in range (QtdExpressoes):
+            path = folderRoot + individual[i] + "\\" + individual[i] + expressoes [j] + ".pgm"
+            PgmImg = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+            ResizedImg = cv2.resize(PgmImg, (Red, Red))
+
+            VectorNormalized = ResizedImg.flatten("F")
+            ROT = -np.ones((QtdIndividuos, 1))
+            ROT[i, 0] = 1
+
+            cv2.imshow("Foto", PgmImg)
+            cv2.waitKey(0)
+
+            VectorNormalized.shape = (len(VectorNormalized), 1)
+            X = np.append(X, VectorNormalized, axis = 1)
+            Y = np.append(Y, ROT, axis = 1)
+
+    return X, Y
 
 ###############################################################################
